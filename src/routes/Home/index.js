1c16501a9865261aa90460f51523b12a38d09011
Ismael Cedano <iocedano@gmail.com>
2021-09-02 21:00:16 -0400

    feat: Added Contact section

--- src/routes/Home/index.js
+++ src/routes/Home/index.js
@@ -3,6 +3,16 @@ import "./styled.css";
 import Header from "../../components/Header";
 import WorkSection from './WorkSection';
 import SkillSection from "./SkillSection";
+import ContactSection from "./ContactSection";
+
+
+const Contact = {
+  email: 'iocedano@gmail.com',
+  points: [
+    { type: 'github', url: 'https://github.com/iocedano' },
+    { type: 'linkedin', url: 'https://www.linkedin.com/in/iocedano' },
+  ]
+};
 
 const skills = [
   { type: 'react' },
@@ -53,6 +63,7 @@ function App() {
       <Header />
       <WorkSection jobs={Jobs} />
       <SkillSection skills={skills} />
+      <ContactSection points={Contact.points} email={Contact.email} />
     </div>
   );
 }
