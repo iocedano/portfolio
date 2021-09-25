dd9679634026532024bf83dece4846b60e82090e
Ismael Cedano <iocedano@gmail.com>
2021-09-25 12:07:21 -0400

    Update - before -  SSR

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import "./styled.css";
+
+const Header = () => (
+  <header className="header-container">
+    <nav className="nav-container">
+      <ul className="nav-list">
+        <li className="nav-item">Job Histories</li>
+        <li className="nav-item">Skills</li>
+        <li className="nav-item">Contact</li>
+      </ul>
+    </nav>
+  </header>
+);
+
+export default Header;
